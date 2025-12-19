cbe70ffd96bb3d3cf0711b8ae677f83fb02aa989
Devansh-kumar07 <kumardevansh12345@gmail.com>
2025-12-19 11:49:11 +0530

    leetcode day 1

---
+++ core-java-practice/leet-code-codebase/Palindrome.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int original = x, reversed = 0;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
